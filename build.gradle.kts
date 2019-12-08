e5e1bee0ef7e938114d34b5df43af9460c8bc28f
Niclas Roßberger <niclas@nidomiro.de>
2019-12-08 12:00:13 +0100

    adds deployment to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
+import java.util.*
+
+
+val myVcsUrl = "https://github.com/nidomiro/KDataLoader"
+val myVcsConnection = "scm:git:git://github.com/nidomiro/KDataLoader"
+val myWebsiteUrl = "https://github.com/nidomiro/KDataLoader"
+
 plugins {
+    `build-scan`
     kotlin("jvm") version "1.3.60"
+    id("com.jfrog.bintray") version "1.8.4"
+    `maven-publish`
 }
 
 group = "de.nidomiro"
-version = "0.1-SNAPSHOT"
+version = "0.1"
 
 repositories {
     // Use jcenter for resolving your dependencies.
@@ -38,5 +48,81 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
+
+
+
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+
+    publishAlways()
+}
+
+
+
+bintray {
+    user = if (project.hasProperty("bintray_user")) project.property("bintray_user") as String else ""
+    key = if (project.hasProperty("bintray_key")) project.property("bintray_key") as String else ""
+    publish = true
+    setPublications("MyPublication")
+    //setConfigurations("archives")
+
+    pkg.apply {
+        repo = "maven"
+        name = project.name
+        setLicenses("MIT")
+        setLabels("kotlin", "Kotlin", "DataLoader", "GraphQL")
+        vcsUrl = myVcsUrl
+        websiteUrl = myWebsiteUrl
+        issueTrackerUrl = "$myVcsUrl/issues"
+
+        version.apply {
+            name = project.version.toString()
+            released = Date().toString()
+        }
+    }
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("MyPublication") {
+            from(components["java"])
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+            artifact(sourcesJar)
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("name", rootProject.name)
+                    appendNode("url", myWebsiteUrl)
+                    appendNode("licenses")
+                        .appendNode("license").apply {
+                            appendNode("name", "MIT")
+                            appendNode("url", "https://opensource.org/licenses/MIT")
+                            appendNode("distribution", "repo")
+                        }
+                    appendNode("developers")
+                        .appendNode("developer").apply {
+                            appendNode("id", "nidomiro")
+                            appendNode("name", "Niclas Roßberger")
+                        }
+                    appendNode("scm").apply {
+                        appendNode("url", myVcsUrl)
+                        appendNode("connection", myVcsConnection)
+                    }
+                }
+            }
+
+        }
     }
 }
